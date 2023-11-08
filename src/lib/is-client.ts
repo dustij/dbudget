9bcf81c4892680eb3acc89a54f59262bc81993d2
dusti <knightlybyte@gmail.com>
2023-11-08 06:50:06 -0700

    Removed unused files

--- src/lib/is-client.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Checks if the code is running on the client-side (browser).
- * Returns true if the code is running on the client-side (browser), otherwise returns false.
- */
-export function isClient() {
-  return typeof window !== "undefined"
-}
